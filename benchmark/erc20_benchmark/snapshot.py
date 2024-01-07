81e1a0dd1b465b268b370be4487825ba305a0a49
cairo <101215230+cairoeth@users.noreply.github.com>
2024-01-07 21:04:05 +0100

    ⚡️ Improved ERC20 benchmark script

--- benchmark/erc20_benchmark/snapshot.py
+++ benchmark/erc20_benchmark/snapshot.py
@@ -1,8 +1,9 @@
-import sys
 import subprocess
 
-address = sys.argv[1]
-snapshot = open('.gas-snapshot', 'w')
+# Deploy contract and get address
+contract = subprocess.run(['cargo', 'stylus', 'deploy', '-e', 'http://localhost:8547', '--private-key', '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'], stdout=subprocess.PIPE).stdout.decode("utf-8")
+location = contract.find('Base')
+address = contract[location-49:location-7]
 
 functions = {
     'name()': ['(string)'],
@@ -19,6 +20,7 @@ functions = {
     # 'domainSeparator()': ['(bytes32)'],
 }
 
+# Run estimates and write to snapshot file
 with open('.gas-snapshot', 'w') as snapshot:
     for function, args in functions.items():
         gas = subprocess.run(['cast', 'estimate', address, function + args[0]] + args[1:] + ['--rpc-url', 'http://localhost:8547'], stdout=subprocess.PIPE).stdout.decode("utf-8")[:-1]
